412dd453c09c8b471310a7a98e2fc3f09fe1adb1
Trevor Lezynski <tlezynski@gmail.com>
2018-12-17 09:30:52 -0500

    Day 2 completed

--- day 2/day2.py
+++ day 2/day2.py
@@ -1,9 +1,14 @@
-
-def checkSum(fileName):
+def readFromFile(fileName):
     file = open(fileName)
     lst = []
     for line in file:
         lst.append(line.strip())
+    return lst
+
+
+# PART 1:
+def checkSum(fileName):
+    lst = readFromFile(fileName)
 
     twice = 0
     thrice = 0
@@ -23,9 +28,39 @@ def checkSum(fileName):
             elif d[k] == 3 and not counted_thrice:
                 counted_thrice = True
                 thrice += 1
-
     return twice * thrice
+# print(checkSum("test.txt"))
+# print(checkSum("input.txt"))
 
-print(checkSum("test.txt"))
 
-print(checkSum("input.txt"))
+# Part 2:
+
+def checkString(stringA, stringB):
+    unMatchingIndex = 0
+    if stringA == stringB or len(stringA) != len(stringB):
+        return False, unMatchingIndex
+
+    numMatching = 0
+    for i in range(0, len(stringA)):
+        if stringA[i] == stringB[i]:
+            numMatching += 1
+        else:
+            unMatchingIndex = i
+    if numMatching == len(stringA) - 1:
+        return True, unMatchingIndex
+    return False, unMatchingIndex
+
+def matchIDs(fileName):
+    lst = readFromFile(fileName)
+    for i in range(0, len(lst)):
+        for j in range(0, len(lst)):
+            checked, strIDX = checkString(lst[i], lst[j])
+            if checked:
+                s = lst[i]
+                sA = s[0:strIDX]
+                sB = s[strIDX + 1:]
+                return sA + sB
+
+
+print(matchIDs("input.txt"))
+
